e05f8798aacca16124b00da54773748d109276c6
lucasSiqueira15 <lucas.sud@outlook.com>
2022-10-06 21:57:01 -0300

    Atualização

--- src/main/java/femass/teste/mainTeste.java
+++ src/main/java/femass/teste/mainTeste.java
@@ -3,8 +3,16 @@ package femass.teste;
 import femass.dao.AutorDao;
 import femass.model.Autor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class mainTeste {
     public static void main(String[] args) {
+        lerDados();
+
+    }
+
+    private static void gravarDados(){
         AutorDao teste = new AutorDao();
         Autor a1 = new Autor("Lucas", "Silva", "Brasil");
         Autor a2 = new Autor("Cintia", "Barros", "Chile");
@@ -14,8 +22,21 @@ public class mainTeste {
             teste.gravar(a2);
         }
         catch (Exception erro){
-            erro.getMessage();
+            System.out.println(erro.getMessage());;
         }
+    }
 
+    private static void lerDados(){
+        AutorDao teste = new AutorDao();
+        List<Autor> testeAutores = new ArrayList<Autor>();
+        try{
+            testeAutores = teste.consultar();
+            for(Autor a: testeAutores){
+                System.out.println(a);
+            }
+        }
+        catch (Exception erro){
+            System.out.println(erro.getMessage());;
+        }
     }
 }
